f6b380cf5507a50f712ea0ea179f7d89c13e465a
dipoisola <isolaoluwatosin@gmail.com>
2019-11-28 12:25:42 -0500

    Completed module

--- stats/data.py
+++ stats/data.py
@@ -9,7 +9,7 @@ game_frames = []
 for game_file in game_files:
     game_frame = pd.read_csv(game_file, names = ['type', 'multi2', 'multi3', 'multi4', 'multi5', 'multi6', 'event'])
     game_frames.append(game_frame)
-    # print(game_frame)
+
 games = pd.concat(game_frames)
 games.loc[games['multi5'] == '??', ['multi5']] = ''
 
@@ -21,5 +21,3 @@ games = pd.concat([games, identifiers], axis=1, sort=False)
 games = games.fillna(' ')
 
 games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'])
-
-# print(games.head())
